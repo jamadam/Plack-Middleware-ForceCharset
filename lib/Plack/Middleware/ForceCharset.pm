effc7b92b66f59944e6c45f0d6a00bcbd249e095
sugama <sugama@sv02.cutout.biz>
2011-06-23 20:48:22 +0900

    pod intialized

--- lib/Plack/Middleware/ForceCharset.pm
+++ lib/Plack/Middleware/ForceCharset.pm
@@ -37,3 +37,28 @@ sub call {
 
 1;
 __END__
+
+=head1 NAME
+
+Plack::Middleware::ForceCharset
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=head2 call
+
+=head1 AUTHOR
+
+Sugama Keita, E<lt>sugama@jamadam.comE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2011 by Sugama Keita.
+
+This program is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself.
+
+=cut
