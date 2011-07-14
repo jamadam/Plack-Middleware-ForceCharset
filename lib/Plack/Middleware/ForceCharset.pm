81f58c9547170aeb31789a406d5364278b698a22
sugama <sugama@sv02.cutout.biz>
2011-07-14 10:26:31 +0900

    Got safer

--- lib/Plack/Middleware/ForceCharset.pm
+++ lib/Plack/Middleware/ForceCharset.pm
@@ -16,22 +16,23 @@ sub call {
     $self->response_cb($self->app->($env), sub {
         my $res = shift;
         my $h = Plack::Util::headers($res->[1]);
-        my $charset_from = 'UTF-8';
-        my $charset_to = $self->charset;
-        my $ct = $h->get('Content-Type');
-        if ($ct =~ s{;?\s*charset=([^;\$]+)}{}) {
-            $charset_from = $1;
+        if (my $ct = $h->get('Content-Type')) {
+            my $charset_from = 'UTF-8';
+            my $charset_to = $self->charset;
+            if ($ct =~ s{;?\s*charset=([^;\$]+)}{}) {
+                $charset_from = $1;
+            }
+            if ($ct =~ qr{^text/}) {
+                $h->set('Content-Type', $ct. ';charset='. $charset_to);
+                my $fixed_body = [];
+                Plack::Util::foreach($res->[2], sub {
+                    Encode::from_to($_[0], $charset_from, $charset_to);
+                    push @$fixed_body, $_[0];
+                });
+                $res->[2] = $fixed_body;
+                $h->set('Content-Length', length $fixed_body);
+            }
         }
-        if ($ct =~ qr{^text/}) {
-            $h->set('Content-Type', $ct. ';charset='. $charset_to);
-        }
-        my $fixed_body = [];
-        Plack::Util::foreach($res->[2], sub {
-            Encode::from_to($_[0], $charset_from, $charset_to);
-            push @$fixed_body, $_[0];
-        });
-        $res->[2] = $fixed_body;
-        $h->set('Content-Length', length $fixed_body);
     });
 }
 
